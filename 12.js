84c6df5f87f84916ff7a376985ab81e4d5d52009
Karthik K S <128198762+karthik-the-legend@users.noreply.github.com>
2025-01-14 19:26:16 +0530

    Update 12.js

---
+++ 12.js
@@ -0,0 +1,12 @@
+function arr(arr1,arr2)
+    {
+        let sum=0;
+        for(let i=0;i<arr1.length;i++)
+        {
+            const arr1value= arr1[i];
+            const arr2value= arr2[i];
+            const addedvalue =arr1value +arr2value;
+            sum=sum+ addedvalue;
+        }
+        return sum;
+    }
