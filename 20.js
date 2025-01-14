f0ee92e13a8fbf3bfb39b3fd6e7f1a4a44f54d29
Karthik K S <128198762+karthik-the-legend@users.noreply.github.com>
2025-01-14 19:29:23 +0530

    Update 20.js

---
+++ 20.js
@@ -0,0 +1,8 @@
+function greet(name)
+          {
+            if(name=== "johnny")
+            {
+                return "hello,my love!";
+            }
+            return "hello,"+name+"!";
+          }
