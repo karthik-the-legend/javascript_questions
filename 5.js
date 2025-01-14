251b6462fe564b004eed2b67083ee8f724eec8d7
Karthik K S <128198762+karthik-the-legend@users.noreply.github.com>
2025-01-14 19:32:44 +0530

    Update 5.js

---
+++ 5.js
@@ -0,0 +1,12 @@
+function isdivisble(n,x,y)
+{
+    const result1 =n/x;
+    const result2 =n/y;
+    if(result %1 ===0 && result2 % 1==0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+    }
