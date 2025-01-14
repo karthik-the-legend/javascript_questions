c3d8fdda92a07e009f57cd9b288d5b671a9add5e
Karthik K S <128198762+karthik-the-legend@users.noreply.github.com>
2025-01-14 19:27:20 +0530

    Update 15.js

---
+++ 15.js
@@ -0,0 +1,9 @@
+function repeatstr(numberofrepeats,str)
+        {
+            let starterstring=' ';
+            for(let i=0;i<numberofrepeats;i++)
+            {
+                starterstring =starterstring + str;
+            }
+            return starterstring;
+        }
